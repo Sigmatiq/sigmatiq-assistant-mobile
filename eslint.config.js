a1e8f144c26bb6867b1926291553de9c3a0b6cba
monikasrivas1 <monikasrivas1@gmail.com>
2025-09-08 04:00:51 -0400

    ci: relax eslint rules for dev (temporarily warn on any/unused-vars/hooks)

--- eslint.config.js
+++ eslint.config.js
@@ -15,6 +15,12 @@ export default tseslint.config([
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn', { 'argsIgnorePattern': '^_', 'varsIgnorePattern': '^_', 'caughtErrors': 'none' }],
+      'react-hooks/rules-of-hooks': 'warn',
+      'react-hooks/exhaustive-deps': 'warn'
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
